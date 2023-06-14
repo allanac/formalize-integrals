e98119dfcfca567d9558a9b5068edb7e6ac72ed3
Julian Black <133246902+MrJulianBlack@users.noreply.github.com>
2023-06-14 04:14:50 +0200

    change: Extended proof of F being measurable

--- EulersMethod/Claim1.lean
+++ EulersMethod/Claim1.lean
@@ -4,38 +4,82 @@ open MeasureTheory
 variable {E: Type _} [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
 variable (F : E → E)
 variable (x₀ : E)
+variable (ε : ℝ)
 
 variable (M : ℝ) (M_nn : 0 ≤ M)
 variable (F_bdd : ∀ e : E, ‖F e‖ ≤ M)
+variable (epos : 0 < ε)
 
 
 #check abs (α := ℝ)
 #check norm
 #check Set.Ico
 
-def meas_a (ε t₀ t₁ : ℝ) (m₀ n₀ : ℕ) (k : ℕ) : ℝ :=
-if k = m₀ then 
-  t₁
-else if k = n₀ then
-  t₀
-else
-  k*ε
+def meas_a (ε : ℝ) (k : ℕ) : ℝ := k*ε
 
 variable (r : ℝ)
 
 #check |r|
+theorem my_integrable_congr{E : Type _} {F : Type _} [NormedAddCommGroup E] {f g: ℝ → E} {a : ℝ} {b : ℝ} {μ : MeasureTheory.Measure ℝ} (he : Set.EqOn f g (Set.Ico a b)) (hg : IntervalIntegrable g μ a b) : IntervalIntegrable f μ a b := by sorry
 
+lemma Fy_measurable : ∀ (t₁ t₀  : ℝ) {htt : t₁ < t₀} { hlb : 0 ≤ t₁}, IntervalIntegrable (fun s => F (y F x₀ ε s)) volume t₁ t₀ := by
+  intro t₁ t₀ htt hlb
+  let a := Nat.floor (t₁/ε)
+  let b := Nat.ceil (t₀/ε)
+  have subset_t : Set.uIoc t₁ t₀ ⊆ (Set.uIoc (a*ε) (b*ε) ) := by
+    apply Set.Ioc_subset_Ioc
+    · apply min_le_of_left_le
+      rw [min_eq_left_iff.mpr (le_of_lt htt)]
+      simp
+      rw [← div_le_div_right epos, mul_div_cancel]
+      apply Nat.floor_le
+      apply div_nonneg hlb
+      apply le_of_lt epos
+      apply ne_of_gt epos
+    · rw [max_eq_right_of_lt htt]
+      apply le_trans _ (le_max_right _ (b*ε))
+      simp
+      rw [← div_le_div_right epos, mul_div_cancel]
+      apply Nat.le_ceil
+      apply ne_of_gt epos
 
-lemma Fy_measurable : ∀ (t₁ t₀  : ℝ), IntervalIntegrable (fun s => F (y F x₀ ε s)) volume t₁ t₀ := by
-  intro t₁ t₀
-  have : t₁ = meas_a ε t₀ t₁ (Nat.floor t₁) (Nat.ceil t₀) (Nat.floor t₁) := by sorry
+  apply IntervalIntegrable.mono_set' _ subset_t
+  have : a * ε = meas_a ε a:= by rfl
   rw [this]
-  have : t₀ = meas_a ε t₀ t₁ (Nat.floor t₁) (Nat.ceil t₀) (Nat.ceil t₀) := by sorry
-  nth_rw 3 [this]
+  have tie :=Nat.floor_lt_ceil_of_lt_of_pos (div_lt_div_of_lt epos htt) (div_pos (lt_of_le_of_lt hlb htt) epos)
+  have : b * ε = meas_a ε b := by rfl
+  rw [this]
   apply IntervalIntegrable.trans_iterate_Ico
-  sorry
-  sorry
-
+  · norm_num
+    linarith
+  intro k hk
+  rw [meas_a, meas_a]
+  let h := (fun (s : ℝ) => F (y F x₀ ε (k*ε)))
+  let f := (fun (s : ℝ) => F (y F x₀ ε s))
+  have y_const : ∀ s ∈ Set.Ico (k*ε) ((k+1)*ε), f s = h s := by
+    intro s hs
+    have hse : s/ε ∈ Set.Ico (k : ℝ) (k+1 : ℝ) := by
+      simp at *
+      rw [← mul_le_mul_right epos]
+      rw [div_mul_cancel]
+      rw [← mul_lt_mul_right epos]
+      rw [div_mul_cancel]
+      tauto
+      apply ne_of_gt epos
+      apply ne_of_gt epos
+      
+    simp
+    rw [y,y,N,N]
+    rw [mul_div_cancel]
+    rw [Nat.floor_eq_on_Ico _ _ hse]
+    rw [Nat.floor_coe]
+    apply ne_of_gt epos
+  have : Set.EqOn f h (Set.Ico (k*ε) ((k+1)*ε)) := y_const
+  have g_integrable : (IntervalIntegrable h volume (k*ε) ((k+1)*ε)) := by sorry
+  apply my_integrable_congr _ g_integrable
+  
+  · sorry
+  · apply this
 #check Fy_measurable
 
 lemma Claim1 : ∀ (ε : NNReal) (t₀ t₁ : ℝ), ‖x F x₀ ε t₀ - x F x₀ ε t₁‖ ≤ M * |t₀ - t₁| := by
