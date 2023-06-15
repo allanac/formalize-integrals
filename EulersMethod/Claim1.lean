8ab62b15cccea13ee688be4286eb7fc169ec6b2c
8080509 <33876023+8080509@users.noreply.github.com>
2023-06-15 00:39:08 -0500

    Further attempts to get the integral formula

--- EulersMethod/Claim1.lean
+++ EulersMethod/Claim1.lean
@@ -1,9 +1,12 @@
 import EulersMethod.Defs
+import Mathlib.MeasureTheory.Integral.FundThmCalculus
 
 open MeasureTheory
 variable {E: Type _} [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
 variable (F : E → E)
 variable (x₀ : E)
+variable (ε : ℝ)
+variable (ε_pos : 0 < ε)
 
 variable (M : ℝ) (M_nn : 0 ≤ M)
 variable (F_bdd : ∀ e : E, ‖F e‖ ≤ M)
@@ -17,24 +20,55 @@ private def meas_a (ε : ℝ) (k : ℕ) : ℝ := k*ε
 
 variable (r : ℝ)
 
+#check max
+#check max_eq_right_of_lt
+theorem max_eq_right_of_le {α} [LinearOrder α] {a b : α} : a ≤ b → max a b = b := by
+  intro h
+  rcases eq_or_lt_of_le h with (_ | h)
+  simp; assumption
+  exact max_eq_right_of_lt h
+
+#check Nat.floor_lt_ceil_of_lt_of_pos
+
+theorem floor_le_ceil_of_le {a b : ℝ} : a ≤ b → ⌊a⌋₊ ≤ ⌈b⌉₊ := by
+  intro h
+  rcases lt_or_le 0 b with (bpos | bnp)
+  rcases lt_or_eq_of_le h with (h | h)
+  apply le_of_lt
+  apply Nat.floor_lt_ceil_of_lt_of_pos
+  assumption
+  assumption
+  rw [h]
+  apply Nat.floor_le_ceil
+  have : ⌊a⌋₊ = 0 := by
+    simp
+    calc
+    a ≤ b := h
+    _ ≤ 0 := bnp
+    _ < _ := by norm_num
+  rw [this]
+  apply zero_le
+
 #check |r|
 theorem my_integrable_congr {EE : Type _} [NormedAddCommGroup EE] {f g: ℝ → EE} {a : ℝ} {b : ℝ} {μ : MeasureTheory.Measure ℝ} (he : Set.EqOn f g (Set.Ico a b)) (hg : IntervalIntegrable g μ a b) : IntervalIntegrable f μ a b := by sorry
 
-lemma Fy_measurable : ∀ (t₁ t₀ ε : ℝ) (_ : 0 < ε) (_ : t₁ < t₀) (_ : 0 ≤ t₁), IntervalIntegrable (fun (s : ℝ) => F (y F x₀ ε s)) volume t₁ t₀ := by
+lemma Fy_measurable : ∀ (t₁ t₀ ε : ℝ) (_ : 0 < ε) (_ : t₁ ≤ t₀) (_ : 0 ≤ t₁), IntervalIntegrable (fun (s : ℝ) => F (y F x₀ ε s)) volume t₁ t₀ := by
   intro t₁ t₀ ε epos htt hlb
+  have enn : 0 ≤ ε := by linarith
+  -- rcases lt_or_eq_of_le htt' with (htt | htt)
   let a := Nat.floor (t₁/ε)
   let b := Nat.ceil (t₀/ε)
   have subset_t : Set.uIoc t₁ t₀ ⊆ (Set.uIoc (a*ε) (b*ε) ) := by
     apply Set.Ioc_subset_Ioc
     · apply min_le_of_left_le
-      rw [min_eq_left_iff.mpr (le_of_lt htt)]
+      rw [min_eq_left_iff.mpr (htt)]
       simp
       rw [← div_le_div_right epos, mul_div_cancel]
       apply Nat.floor_le
       apply div_nonneg hlb
       apply le_of_lt epos
       apply ne_of_gt epos
-    · rw [max_eq_right_of_lt htt]
+    · rw [max_eq_right_of_le htt]
       apply le_trans _ (le_max_right _ (b*ε))
       simp
       rw [← div_le_div_right epos, mul_div_cancel]
@@ -43,7 +77,8 @@ lemma Fy_measurable : ∀ (t₁ t₀ ε : ℝ) (_ : 0 < ε) (_ : t₁ < t₀) (_
   apply IntervalIntegrable.mono_set' _ subset_t
   have : a * ε = meas_a ε a:= by rfl
   rw [this]
-  have tie :=Nat.floor_lt_ceil_of_lt_of_pos (div_lt_div_of_lt epos htt) (div_pos (lt_of_le_of_lt hlb htt) epos)
+  -- have tie := Nat.floor_le_ceil_of_le (div_lt_div_of_lt epos htt) (div_pos (lt_of_le_of_lt hlb htt) epos)
+  have tie : ⌊t₁ / ε⌋₊ ≤ ⌈t₀ / ε⌉₊ := floor_le_ceil_of_le (div_le_div_of_le enn htt)
   have : b * ε = meas_a ε b := by rfl
   rw [this]
   apply IntervalIntegrable.trans_iterate_Ico
@@ -77,10 +112,100 @@ lemma Fy_measurable : ∀ (t₁ t₀ ε : ℝ) (_ : 0 < ε) (_ : t₁ < t₀) (_
   simp at *
   exact more_bullshit
 
+lemma Fy_measurable' : ∀ (t₁ t₀ ε : ℝ) (_ : 0 < ε) (_ : 0 ≤ t₀) (_ : 0 ≤ t₁), IntervalIntegrable (fun (s : ℝ) => F (y F x₀ ε s)) volume t₁ t₀ := by
+  intros t₁ t₀ ε ε_pos t0nn t1nn
+  rcases le_total t₀ t₁ with (h01 | h10)
+  symm
+  apply Fy_measurable <;> assumption
+  apply Fy_measurable <;> assumption
+
 #check Fy_measurable
 
-lemma Claim1 : ∀ (ε : ℝ) {_ : 0 < ε} (t₀ t₁ : ℝ) {_ : 0 < t₁} {_ : 0 < t₀}, ‖x F x₀ ε t₀ - x F x₀ ε t₁‖ ≤ M * |t₀ - t₁| := by
-  intro ε epos t₀ t₁ t1pos t0pos
+lemma ftc_on_x : ∀ (N₀ : ℕ) t, 0 ≤ t → t ≤ N₀ * ε → x F x₀ ε t - x₀ = ∫ (s : ℝ) in (0)..(t), F (y F x₀ ε s) := by
+  intro N₀
+  induction' N₀ with N₀ ih
+  intro t tnn tnp
+  simp at tnp
+  have : t = 0 := by linarith
+  rw [this]
+  simp [x]
+  unfold x_N
+  have : N ε 0 = 0 := by
+    unfold N
+    simp
+  rw [this]
+  simp
+  left; left
+  unfold lam
+  simp [this]
+  intro t tnn tub
+  rcases le_or_lt t (N₀ * ε) with (tub' | tlb)
+  . specialize ih t tnn tub'
+    exact ih
+  specialize ih (N₀ * ε) _ _
+  apply mul_nonneg
+  simp
+  linarith
+  linarith
+  have : x F x₀ ε t - x₀ = (x F x₀ ε t - x F x₀ ε (↑N₀ * ε)) + (x F x₀ ε (↑N₀ * ε) - x₀) := by simp
+  rw [this, ih]
+  have : (∫ (s : ℝ) in (0)..(t), F (y F x₀ ε s)) = (∫ (s : ℝ) in (N₀ * ε)..(t), F (y F x₀ ε s)) + (∫ (s : ℝ) in (0)..(N₀ * ε), F (y F x₀ ε s)) := by
+    -- intervalIntegral
+    rw [add_comm, intervalIntegral.integral_add_adjacent_intervals]
+    apply Fy_measurable
+    exact ε_pos
+    apply mul_nonneg
+    simp
+    linarith
+    norm_num
+    apply Fy_measurable
+    apply ε_pos
+    apply le_of_lt tlb
+    apply mul_nonneg
+    norm_num
+    exact le_of_lt ε_pos
+  rw [this]
+  rw [add_right_cancel_iff]
+  symm
+  have h₀ : t ∈ closed_ival N₀ ε := by
+    constructor
+    linarith
+    simp at tub
+    assumption
+  have h₁ : N₀ * ε ∈ closed_ival N₀ ε := by
+    constructor
+    simp
+    apply mul_le_mul_of_nonneg_right
+    norm_num
+    linarith
+  rw [← x_cong_x_alt_closed (th := h₀), ← x_cong_x_alt_closed (th := h₁)]
+  unfold intervalIntegral
+  -- rw [MeasureTheory.integral_Ioc_eq_integral_Ioo]
+  sorry
+  -- apply intervalIntegral.integral_eq_sub_of_hasDerivAt_of_tendsto (f := x_alt F x₀ ε N₀)
+  -- exact tlb
+  -- intro u uh
+  -- apply deriv_x_alt_eq
+  -- apply ε_pos
+  -- apply le_trans (b := N₀ * ε)
+  -- apply nat_ep_nonneg ε_pos
+  -- rw [Set.mem_Ioo] at uh
+  -- apply le_of_lt uh.left
+  -- apply not_bad_of_good (N₀ := N₀)
+  -- rcases uh with ⟨ulb, uub⟩
+  -- apply Set.mem_Ioo.mpr; refine' ⟨ulb, _⟩
+  -- apply lt_of_lt_of_le uub
+  -- simp at tub
+  -- assumption
+  -- apply Fy_measurable
+  -- apply ε_pos
+  -- apply le_of_lt tlb
+  -- apply nat_ep_nonneg ε_pos
+  sorry
+  sorry
+
+lemma Claim1 : ∀ (ε : ℝ) {_ : 0 < ε} (t₀ t₁ : ℝ) {_ : 0 ≤ t₁} {_ : 0 ≤ t₀}, ‖x F x₀ ε t₀ - x F x₀ ε t₁‖ ≤ M * |t₀ - t₁| := by
+  intro ε epos t₀ t₁ t1nn t0nn
   calc
     ‖x F x₀ ε t₀ - x F x₀ ε t₁‖ = 
      ‖ x₀ + (∫ (s : ℝ) in (0)..(t₀), F (y F x₀ ε s) ) - x₀ - (∫ (s : ℝ) in (0)..(t₁), F (y F x₀ ε s) )‖ := by sorry
@@ -89,8 +214,8 @@ lemma Claim1 : ∀ (ε : ℝ) {_ : 0 < ε} (t₀ t₁ : ℝ) {_ : 0 < t₁} {_ :
     _ = ‖(∫ (s : ℝ) in (t₁)..(t₀), F (y F x₀ ε s) )‖ := by
         rw [add_comm,intervalIntegral.integral_add_adjacent_intervals] 
         · apply IntervalIntegrable.symm
-          apply Fy_measurable F x₀ 0 t₁ ε epos t1pos (le_refl 0)
-        · apply Fy_measurable F x₀ 0 t₀ ε epos t0pos (le_refl 0)
+          apply Fy_measurable F x₀ 0 t₁ ε epos t1nn (le_refl 0)
+        · apply Fy_measurable F x₀ 0 t₀ ε epos t0nn (le_refl 0)
     _ ≤ M * abs (t₀ - t₁) := by
       apply intervalIntegral.norm_integral_le_of_norm_le_const
       intro _ _
--- EulersMethod/Defs.lean
+++ EulersMethod/Defs.lean
@@ -11,6 +11,8 @@ import Mathlib.MeasureTheory.Measure.MeasureSpace
 import Mathlib.Analysis.Calculus.Deriv.Mul
 import Mathlib.Analysis.Calculus.Deriv.Add
 import Mathlib.Topology.Filter
+import Mathlib.MeasureTheory.Integral.FundThmCalculus
+import Mathlib.MeasureTheory.Integral.DivergenceTheorem
 
 variable {E: Type _} [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
 variable (F : E → E)
@@ -280,97 +282,193 @@ theorem deriv_x_alt_eq (N₀ : ℕ) : ∀ t : ℝ, HasDerivAt (x_alt F x₀ ε N
 #check HasDerivAt.congr_of_eventuallyEq
 #check Filter.mem_of_superset
 
-theorem x_cong_x_alt (N₀ : ℕ) (t : ℝ) (th : (Set.Ioo (N₀ * ε) ((N₀ + 1) * ε) : Set ℝ) t) : x_alt F x₀ ε N₀ =ᶠ[nhds t] x F x₀ ε := by
-  let u : Set ℝ := Set.Ioo (N₀ * ε) ((N₀ + 1) * ε)
+def good_ival (N₀ : ℕ) (ε : ℝ) : Set ℝ := Set.Ioo (N₀ * ε) ((N₀ + 1) * ε)
+-- def sgood_left (N₀ : ℕ) (ε : ℝ) : Set ℝ := Set.Ico (N₀ * ε) ((N₀ + 1) * ε)
+-- def sgood_right (N₀ : ℕ) (ε : ℝ) : Set ℝ := Set.Ioc (N₀ * ε) ((N₀ + 1) * ε)
+def closed_ival (N₀ : ℕ) (ε : ℝ) : Set ℝ := Set.Icc (N₀ * ε) ((N₀ + 1) * ε)
+
+#check Disjoint
+
+-- #check mul_lt_mul_of_pos_iff
+
+lemma good_bad_disjoint (N₀ : ℕ) (ε : ℝ) : Disjoint (good_ival N₀ ε) (bad_set ε) := by
+  -- intro ε_pos
+  rw [Set.disjoint_iff]
+  rintro t ⟨good, bad⟩
+  unfold bad_set at bad
+  rw [Set.mem_image] at bad
+  rcases bad with ⟨x, _, rfl⟩
+  unfold good_ival at good
+  rw [Set.mem_Ioo] at good
+  rcases good with ⟨lb, ub⟩
+  rcases lt_or_le 0 ε with (ε_pos | ε_np)
+  have lb := lt_of_mul_lt_mul_right lb (le_of_lt ε_pos)
+  have ub := lt_of_mul_lt_mul_right ub (le_of_lt ε_pos)
+  norm_cast at lb
+  norm_cast at ub
+  linarith
+  have silly := lt_trans lb ub
+  have : ε ≠ 0 := by
+    intro ε_zero
+    rw [ε_zero] at silly
+    simp at silly
+  have ε_neg : ε < 0 := lt_of_le_of_ne ε_np this
+  have lb := le_of_lt lb
+  have ub := le_of_lt ub
+  have : N₀ * ε ≤ (N₀ + 1) * ε := by apply le_trans (b := x * ε) <;> assumption
+  have : N₀ * ε < (N₀ + 1) * ε := by
+    apply lt_of_le_of_ne
+    exact this
+    intro h
+    have := mul_right_cancel₀ ?_ h
+    norm_num at this
+    assumption
+  have : (N₀ + 1) * ε ≤ N₀ * ε := by
+    apply mul_le_mul_of_nonpos_right
+    norm_num
+    assumption
+  linarith
+
+#check lt_of_mul_lt_mul_of_nonpos_right
+
+lemma not_bad_of_good : ∀ {N₀ : ℕ} {ε t : ℝ}, t ∈ good_ival N₀ ε → t ∉ bad_set ε := by
+  intro N₀ ε t
+  have := good_bad_disjoint
+  intro good bad
+  apply this N₀ ε (x := Set.singleton t)
+  rintro t rfl; assumption
+  rintro t rfl; assumption
+  apply Set.mem_singleton
+
+lemma nat_ep_nonneg {N₀ : ℕ} {ε : ℝ} : 0 < ε → 0 ≤ N₀ * ε := by
+  intro ε_pos
+  apply mul_nonneg
+  simp
+  linarith
+
+theorem x_cong_x_alt_closed {N₀ : ℕ} {t : ℝ} (th : (closed_ival N₀ ε) t) : x_alt F x₀ ε N₀ t = x F x₀ ε t := by
+  rcases th with ⟨tlb, tub⟩
+  rcases eq_or_lt_of_le tub with (teq | tub)
+    -- simp
+  unfold x x_alt
+  simp
+  have : N ε t = N₀ + 1 := by
+    rw [N, Nat.floor_eq_iff, teq]
+    rw [mul_div_cancel]
+    simp
+    linarith
+    apply div_nonneg
+    apply le_trans _ tlb
+    apply nat_ep_nonneg ε_pos
+    linarith
+  unfold lam y lam_alt
+  rw [this]
+  rw [teq, x_N]
+  rw [mul_div_cancel]
+  simp
+  linarith
+  have N_eq : N ε t = N₀ := by
+    rw [N, Nat.floor_eq_iff]
+    constructor
+    calc
+      N₀ = N₀ * ε / ε := by
+        symm
+        apply mul_div_cancel
+        linarith
+      _ ≤ _ := by
+        rw [div_le_div_right]
+        linarith
+        assumption
+    have : (N₀ + 1) = (N₀ + 1) * ε / ε := by symm; apply mul_div_cancel; linarith
+    rw [this]
+    rw [div_lt_div_right]
+    exact tub
+    exact ε_pos
+    apply div_nonneg
+    apply le_trans (b := N₀ * ε)
+    apply nat_ep_nonneg
+    exact ε_pos
+    exact tlb
+    apply le_of_lt ε_pos
+  unfold x x_alt y lam lam_alt
+  rw [N_eq]
+    --   apply mul_nonneg
+    --   simp
+    --   linarith
+    --   linarith
+    --   linarith
+  -- unfold lam y lam_alt
+  -- rw [this]
+  -- simp
+
+-- theorem x_cong_x_alt' {N₀ : ℕ} {t : ℝ} (th : (good_ival N₀ ε) t) : 
+
+theorem x_cong_x_alt {N₀ : ℕ} {t : ℝ} (th : (good_ival N₀ ε) t) : x_alt F x₀ ε N₀ =ᶠ[nhds t] x F x₀ ε := by
+  let u : Set ℝ := good_ival N₀ ε -- Set.Ioo (N₀ * ε) ((N₀ + 1) * ε)
   apply Filter.mem_of_superset (x := u)
   apply Ioo_mem_nhds
   apply th.left
   apply th.right
-  . rintro t ⟨tlb, tub⟩
+  . rintro t th
     simp
-    unfold x x_alt
-    simp
-    have : N ε t = N₀ := by
-      rw [N, Nat.floor_eq_iff]
-      constructor
-      calc
-        N₀ = N₀ * ε / ε := by
-          symm
-          apply mul_div_cancel
-          linarith
-        _ ≤ _ := by
-          rw [div_le_div_right]
-          linarith
-          assumption
-      have : (N₀ + 1) = (N₀ + 1) * ε / ε := by symm; apply mul_div_cancel; linarith
-      rw [this]
-      rw [div_lt_div_right]
-      exact tub
-      exact ε_pos
-      apply div_nonneg
-      apply le_trans (b := N₀ * ε)
-      apply mul_nonneg
-      simp
-      linarith
-      linarith
-      linarith
-    unfold lam y lam_alt
-    rw [this]
+    apply x_cong_x_alt_closed
+    assumption
+    apply Set.Ioo_subset_Icc_self
+    assumption
 
 #print y
 
 #check HasDerivAt
 #check HasDerivAt.congr_of_eventuallyEq
 
-theorem der_x_eq_Fy (t : ℝ) : (t ∉ bad_set ε) → (HasDerivAt (x F x₀ ε) (F (y F x₀ ε t)) t) := by
-  intro th
+theorem der_x_eq_Fy (t : ℝ) : 0 ≤ t → t ∉ bad_set ε → (HasDerivAt (x F x₀ ε) (F (y F x₀ ε t)) t) := by
+  intro tnn th
   let N₀ := N ε t
   have N0_eq : N₀ = N ε t := rfl
-  rcases le_or_lt 0 t with h | h
-  . let I : Set ℝ := Set.Ioo (N₀ * ε) ((N₀ + 1) * ε)
-    have : t ∈ I := by
-      constructor
-      rw [N0_eq]
-      unfold N
-      apply lt_of_le_of_ne
-      apply mul_le_of_nonneg_of_le_div
-      assumption
+  let I : Set ℝ := Set.Ioo (N₀ * ε) ((N₀ + 1) * ε)
+  have : t ∈ I := by
+    constructor
+    rw [N0_eq]
+    unfold N
+    apply lt_of_le_of_ne
+    apply mul_le_of_nonneg_of_le_div
+    assumption
+    linarith
+    apply Nat.floor_le
+    apply div_nonneg <;> linarith
+    unfold bad_set at th
+    rw [Set.mem_image] at th
+    push_neg at th
+    apply th
+    simp
+    rw [N0_eq]
+    unfold N
+    have : t = (t / ε) * ε := by
+      rw [div_mul_cancel]
       linarith
-      apply Nat.floor_le
-      apply div_nonneg <;> linarith
-      unfold bad_set at th
-      rw [Set.mem_image] at th
-      push_neg at th
-      apply th
-      simp
-      rw [N0_eq]
-      unfold N
-      have : t = (t / ε) * ε := by
-        rw [div_mul_cancel]
-        linarith
-      rw [this]
-      rw [mul_lt_mul_right]
-      rw [← this]
-      apply Nat.lt_floor_add_one
-      assumption
-    unfold y
-    apply HasDerivAt.congr_of_eventuallyEq _ (x_cong_x_alt F x₀ ε ε_pos N₀ t this).symm
-    rw [← N0_eq]
-    apply deriv_x_alt_eq
-    exact ε_pos
-  . have : N ε t = 0 := by
-      unfold N
-      simp
-      calc
-        t / ε < 0 := by
-          apply div_neg_of_neg_of_pos <;> assumption
-        _ < 1 := by norm_num
-    let I : Set ℝ := Set.Iio 0
-    have : t ∈ I := by
-      rw [Set.mem_Iio]
-      exact h
-    unfold y
-    sorry --apply HasDerivAt.congr_of_eventuallyEq _ (x_cong_x_alt F x₀ ε ε_pos N₀ t this).symm
+    rw [this]
+    rw [mul_lt_mul_right]
+    rw [← this]
+    apply Nat.lt_floor_add_one
+    assumption
+  unfold y
+  apply HasDerivAt.congr_of_eventuallyEq _ (x_cong_x_alt F x₀ ε ε_pos this).symm
+  rw [← N0_eq]
+  apply deriv_x_alt_eq
+  exact ε_pos
+  -- . have : N ε t = 0 := by
+  --     unfold N
+  --     simp
+  --     calc
+  --       t / ε < 0 := by
+  --         apply div_neg_of_neg_of_pos <;> assumption
+  --       _ < 1 := by norm_num
+  --   let I : Set ℝ := Set.Iio 0
+  --   have : t ∈ I := by
+  --     rw [Set.mem_Iio]
+  --     exact h
+  --   unfold y
+  --   sorry --apply HasDerivAt.congr_of_eventuallyEq _ (x_cong_x_alt F x₀ ε ε_pos N₀ t this).symm
 
 theorem bad_set_almost_nowhere : volume (bad_set ε) = 0 := by
   apply Set.Countable.measure_zero
@@ -399,6 +497,116 @@ example (t : ℝ) : deriv (fun u => x F x₀ ε u) t = sorry := by
 --   apply ite_ae_eq_of_measure_zero
 --   rw [Filter.EventuallyEq]
 --   rw [ae_iff_comp]
+
+lemma int_deriv_x_eq_int_F_y : ∀ t₀ t₁, 0 ≤ t₀ → t₀ ≤ t₁ → (∫ (s : ℝ) in (t₀)..(t₁), F (y F x₀ ε s)) = (∫ (s : ℝ) in (t₀)..(t₁), deriv (x F x₀ ε) s) := by
+  intros t₀ t₁ t0nn t0_le_t1
+  apply intervalIntegral.integral_congr_ae
+  apply Filter.mem_of_superset (x := (bad_set ε)ᶜ)
+  rw [compl_mem_ae_iff]
+  apply bad_set_almost_nowhere
+  intro s sh
+  dsimp
+  rintro ⟨s_pos, _⟩
+  rw [min_eq_left] at s_pos
+  symm
+  apply HasDerivAt.deriv
+  apply der_x_eq_Fy
+  exact ε_pos
+  linarith
+  exact sh
+  exact t0_le_t1
+
+-- set_option maxHeartbeats 0
+
+open intervalIntegral
+
+example : ∀ (a b c : ℝ) (f : ℝ → ℝ), (∫ (t : ℝ) in (a)..(b), f t) + (∫ (t : ℝ) in (b)..(c), f t) = (∫ (t : ℝ) in (a)..(c), f t) := by
+  intros
+  sorry
+  -- apply integral_add_adjacent_intervals
+  -- library_search
+
+-- -- Just copied the below; can't import due to dependency loop
+-- private def meas_a (ε : ℝ) (k : ℕ) : ℝ := k*ε
+-- theorem my_integrable_congr {EE : Type _} [NormedAddCommGroup EE] {f g: ℝ → EE} {a : ℝ} {b : ℝ} {μ : MeasureTheory.Measure ℝ} (he : Set.EqOn f g (Set.Ico a b)) (hg : IntervalIntegrable g μ a b) : IntervalIntegrable f μ a b := by sorry
+-- lemma Fy_measurable : ∀ (t₁ t₀ ε : ℝ) (_ : 0 < ε) (_ : t₁ < t₀) (_ : 0 ≤ t₁), IntervalIntegrable (fun (s : ℝ) => F (y F x₀ ε s)) volume t₁ t₀ := by
+--   intro t₁ t₀ ε epos htt hlb
+--   let a := Nat.floor (t₁/ε)
+--   let b := Nat.ceil (t₀/ε)
+--   have subset_t : Set.uIoc t₁ t₀ ⊆ (Set.uIoc (a*ε) (b*ε) ) := by
+--     apply Set.Ioc_subset_Ioc
+--     · apply min_le_of_left_le
+--       rw [min_eq_left_iff.mpr (le_of_lt htt)]
+--       simp
+--       rw [← div_le_div_right epos, mul_div_cancel]
+--       apply Nat.floor_le
+--       apply div_nonneg hlb
+--       apply le_of_lt epos
+--       apply ne_of_gt epos
+--     · rw [max_eq_right_of_lt htt]
+--       apply le_trans _ (le_max_right _ (b*ε))
+--       simp
+--       rw [← div_le_div_right epos, mul_div_cancel]
+--       apply Nat.le_ceil
+--       apply ne_of_gt epos
+--   apply IntervalIntegrable.mono_set' _ subset_t
+--   have : a * ε = meas_a ε a:= by rfl
+--   rw [this]
+--   have tie :=Nat.floor_lt_ceil_of_lt_of_pos (div_lt_div_of_lt epos htt) (div_pos (lt_of_le_of_lt hlb htt) epos)
+--   have : b * ε = meas_a ε b := by rfl
+--   rw [this]
+--   apply IntervalIntegrable.trans_iterate_Ico
+--   · norm_num
+--     linarith
+--   intro k hk
+--   rw [meas_a, meas_a]
+--   let h := (fun (_ : ℝ) => F (y F x₀ ε (k*ε)))
+--   let f := (fun (s : ℝ) => F (y F x₀ ε s))
+--   have y_const : ∀ s ∈ Set.Ico (k*ε) ((k+1)*ε), f s = h s := by
+--     intro s hs
+--     have hse : s/ε ∈ Set.Ico (k : ℝ) (k+1 : ℝ) := by
+--       simp at *
+--       rw [← mul_le_mul_right epos]
+--       rw [div_mul_cancel]
+--       rw [← mul_lt_mul_right epos]
+--       rw [div_mul_cancel]
+--       tauto
+--       apply ne_of_gt epos
+--       apply ne_of_gt epos
+--     simp
+--     rw [y,y,N,N]
+--     rw [mul_div_cancel]
+--     rw [Nat.floor_eq_on_Ico _ _ hse]
+--     rw [Nat.floor_coe]
+--     apply ne_of_gt epos
+--   have : Set.EqOn f h (Set.Ico (k*ε) ((k+1)*ε)) := y_const
+--   have g_integrable : IntervalIntegrable h volume (k*ε) ((k+1)*ε) := by 
+--     apply intervalIntegrable_const
+--   have more_bullshit := my_integrable_congr this g_integrable
+--   simp at *
+--   exact more_bullshit
+
+-- lemma x_cts : Continuous (x F x₀ ε) where
+--   isOpen_preimage := by
+--     intro s sh
+--     rw [isOpen_iff_forall_mem_open]
+--     intro t th
+--     rw [Set.mem_preimage] at th
+--     rw [isOpen_iff_open_ball_subset] at sh
+--     by_cases h : t ∈ bad_set ε
+--     . rcases h with ⟨N₀, _, NH⟩
+--       dsimp at NH
+--       rw [← NH] at *
+--     sorry
+
+-- lemma ftc_on_x' : ∀ t₀ t₁ : ℝ, 0 ≤ t₀ → t₀ ≤ t₁ → x F x₀ ε t₁ - x F x₀ ε t₀ = ∫ (s : ℝ) in (t₀)..(t₁), deriv (x F x₀ ε) s := by
+--   intro t₀ t₁ t0nn t1lb
+--   symm
+--   apply MeasureTheory.integral_eq_of_has_deriv_within_at_off_countable (s := bad_set ε)
+--   apply Set.Countable.image
+--   apply Set.to_countable
+  
+--   sorry
 
 lemma piecewise_constant_ode : ∀ N : ℕ, y F x₀ ε (N*ε) = x₀ + ∫ (s : ℝ) in (0)..(N*ε), F (y F x₀ ε s) ∂volume := by
   intro N
