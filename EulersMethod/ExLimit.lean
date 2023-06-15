e08615a7a63ca54ed253d5eb54f61bfd1321fcbb
Julian Black <133246902+MrJulianBlack@users.noreply.github.com>
2023-06-15 07:33:51 +0200

    extended proof

--- EulersMethod/ExLimit.lean
+++ EulersMethod/ExLimit.lean
@@ -12,76 +12,98 @@ open scoped BoundedContinuousFunction
 #check uniformlyBounded
 
 open MeasureTheory
+
+section
+
 variable {E: Type _} [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
 variable (F : E → E)
 variable (x₀ : E)
 #where
 
-variable (M : ℝ) (M_nn : 0 ≤ M)
+variable (M : NNReal)
 variable (F_bdd : ∀ e : E, ‖F e‖ ≤ M)
 
 noncomputable def x_c' (k : ℕ) : (Set.Icc 0 (1 : ℝ)) → E
 | t => x F x₀ (1/((k : ℝ)+1)) (t)
+open ENNReal
+lemma x_is_lipschitz : ∀ (k : ℕ), LipschitzWith M (x_c' F x₀ k) := by
+  intro k
+  rw [lipschitzWith_iff_dist_le_mul]
+  intro z1 z2
+  rw [dist_eq_norm]
+  have : M * dist z1 z2 = M * norm ((z1:ℝ)-(z2:ℝ)) := by sorry
+  rw [this]
+  rw [x_c',x_c']
+  simp
+  have := Claim1 F x₀ M F_bdd (1/((k : ℝ)+1)) (one_div_pos.mpr (Nat.cast_add_one_pos _)) z1 z2 
+  simp at *
+  apply this
+  sorry
+  sorry
+  --· rcases z2 with ⟨z2', z2h⟩
+  --sorry
 
+#check x_is_lipschitz F x₀ M
 --set_option pp.all true
 noncomputable def x_c (k : ℕ) : (Set.Icc 0 (1 : ℝ)) →ᵇ E where
- toFun := x_c' F x₀ k
- map_bounded' := by
-   obtain ⟨C, hc⟩ := uniformlyBounded F x₀ 
-   simp
-   use 2*C
-   intro a ha b hb
-   simp
-   rw [x_c',x_c']
-   norm_num
-   rw [dist_eq_norm]
-   calc
-   _ ≤ ‖x F x₀ (↑k + 1)⁻¹ ↑a‖ + ‖x F x₀ (↑k + 1)⁻¹ ↑b‖ := norm_sub_le _ _
-   _ ≤ C + ‖x F x₀ (↑k + 1)⁻¹ ↑b‖ := by
-     apply add_le_add_right
-     apply hc
-     simp
-     · apply Nat.cast_add_one_pos
-     · tauto
-   _ ≤ C + C := by
-     apply add_le_add_left
-     apply hc
-     simp
-     · apply Nat.cast_add_one_pos
-     · tauto
-   _ ≤ 2*C := by linarith
- continuous_toFun := by sorry
+  toFun := x_c' F x₀ k
+  map_bounded' := by
+    obtain ⟨C, hc⟩ := uniformlyBounded F x₀ 
+    simp
+    use 2*C
+    intro a ha b hb
+    simp
+    rw [x_c',x_c']
+    norm_num
+    rw [dist_eq_norm]
+    calc
+    _ ≤ ‖x F x₀ (↑k + 1)⁻¹ ↑a‖ + ‖x F x₀ (↑k + 1)⁻¹ ↑b‖ := norm_sub_le _ _
+    _ ≤ C + ‖x F x₀ (↑k + 1)⁻¹ ↑b‖ := by
+      apply add_le_add_right
+      apply hc
+      simp
+      · apply Nat.cast_add_one_pos
+      · tauto
+    _ ≤ C + C := by
+      apply add_le_add_left
+      apply hc
+      simp
+      · apply Nat.cast_add_one_pos
+      · tauto
+    _ ≤ 2*C := by linarith
+  continuous_toFun := LipschitzWith.continuous (x_is_lipschitz F x₀ M F_bdd k)
 
 
-#check x_c
+#check x_c F x₀_M
 
 #check x
 
-lemma x_c_eq_cont_at {a : ℝ} (_ : a ∈ Set.Icc 0 1) : EquicontinuousAt (x_c F x₀) a := by sorry
+
+lemma x_c_eq_cont_at  (a : Set.Icc 0 1) : EquicontinuousAt (fun n ↦ (x_c F x₀ M F_bdd n).toFun) a := by sorry
 -- use Claim1 here
 
-lemma x_c_eq_cont : Equicontinuous (x_c F x₀) := by sorry
+lemma x_c_eq_cont : Equicontinuous (fun n ↦ (x_c F x₀ M F_bdd n).toFun) := by sorry
 -- use previous lemma
 
 -- has to be redefined: structures
-def A := Set.range (x_c F x₀) 
+def A := Set.range (x_c F x₀ M F_bdd) 
 
 
 #check A
 
-lemma A_is_compact : IsCompact (A F x₀) := by sorry
+lemma A_is_compact : IsCompact (A F x₀ M F_bdd) := by sorry
 -- need Arzela-Ascoli here
 
-lemma A_is_seq_compact : IsSeqCompact (A F x₀) := IsCompact.isSeqCompact (A_is_compact F x₀)
+lemma A_is_seq_compact : IsSeqCompact (A F x₀ M F_bdd) := IsCompact.isSeqCompact (A_is_compact F x₀ M F_bdd)
 
-def x_exists := ((A_is_seq_compact (x := x_c F x₀) F x₀) (by simp; intro n; rw [A]; aesop))
-noncomputable def x_L := (x_exists F x₀).choose
-def x_L_spec := (x_exists F x₀).choose_spec
+def x_exists := ((A_is_seq_compact (x := x_c F x₀ M F_bdd) F x₀ M F_bdd) (by simp; intro n; rw [A]; aesop))
+noncomputable def x_L := (x_exists F x₀ M F_bdd).choose
+def x_L_spec := (x_exists F x₀ M F_bdd).choose_spec
 
-def x_subseq_exists := (x_L_spec F x₀).right
-noncomputable def x_subseq := (x_subseq_exists F x₀).choose
-#check x_subseq F x₀
-def x_subseq_spec := (x_subseq_exists F x₀).choose_spec
+def x_subseq_exists := (x_L_spec F x₀ M F_bdd).right
+noncomputable def x_subseq := (x_subseq_exists F x₀ M F_bdd).choose
+#check x_subseq F x₀ M F_bdd
+def x_subseq_spec := (x_subseq_exists F x₀ M F_bdd).choose_spec
 
 #check x_L
 #check x_L_spec
